532479de2ea3bf1bbf14bfb13cfbc36f015b90f8
Jacob <akintolajacob75@gmail.com>
2026-03-16 20:54:08 +0100

    feat: add input validation error message

--- script.js
+++ script.js
@@ -4,55 +4,63 @@ let destinationCity;
 function checkDestination() {
     const userAccountBalance = accountBalance.value;
 
-    // Lagos
-    if (userAccountBalance >= 85000) {
-        destinationCity = "Lagos";
-        result.innerHTML = destinationText + "</br>* " + destinationCity;
+    // Check if user input is valid
+    if (userAccountBalance == "") {
+        result.innerHTML = "Please enter a valid balance!";
+        result.style.color = "red";
+    }
 
-        // Benin City | ₦90,000
-        if (userAccountBalance >= 90000) {
-            destinationCity = "Benin City";
-            result.innerHTML += "</br>* " + destinationCity;
+    else {
+        // Lagos
+        if (userAccountBalance >= 85000) {
+            destinationCity = "Lagos";
+            result.innerHTML = destinationText + "</br>* " + destinationCity;
 
-            // Abuja
-            if (userAccountBalance >= 95000) {
-                destinationCity = "Abuja";
+            // Benin City | ₦90,000
+            if (userAccountBalance >= 90000) {
+                destinationCity = "Benin City";
                 result.innerHTML += "</br>* " + destinationCity;
 
-                // Owerri
-                if (userAccountBalance >= 100000) {
-                    destinationCity = "Owerri";
+                // Abuja
+                if (userAccountBalance >= 95000) {
+                    destinationCity = "Abuja";
                     result.innerHTML += "</br>* " + destinationCity;
 
-                    // Enugu
-                    if (userAccountBalance >= 105000) {
-                        destinationCity = "Enugu";
+                    // Owerri
+                    if (userAccountBalance >= 100000) {
+                        destinationCity = "Owerri";
                         result.innerHTML += "</br>* " + destinationCity;
 
-                        // Port Harcourt
-                        if (userAccountBalance >= 110000) {
-                            destinationCity = "Port Harcourt";
+                        // Enugu
+                        if (userAccountBalance >= 105000) {
+                            destinationCity = "Enugu";
                             result.innerHTML += "</br>* " + destinationCity;
 
-                            // Uyo
-                            if (userAccountBalance >= 115000) {
-                                destinationCity = "Uyo";
+                            // Port Harcourt
+                            if (userAccountBalance >= 110000) {
+                                destinationCity = "Port Harcourt";
                                 result.innerHTML += "</br>* " + destinationCity;
 
-                                // Calabar | ₦120,000
-                                if (userAccountBalance >= 120000) {
-                                    destinationCity = "Calabar";
+                                // Uyo
+                                if (userAccountBalance >= 115000) {
+                                    destinationCity = "Uyo";
                                     result.innerHTML += "</br>* " + destinationCity;
+
+                                    // Calabar | ₦120,000
+                                    if (userAccountBalance >= 120000) {
+                                        destinationCity = "Calabar";
+                                        result.innerHTML += "</br>* " + destinationCity;
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+
+        } else {
+            result.innerHTML =
+                "Sorry, you don't have enough money to travel. <br>Just stay at home";
         }
-        
-    } else {
-        result.innerHTML =
-            "Sorry, you don't have enough money to travel. <br>Just stay at home";
     }
 }
