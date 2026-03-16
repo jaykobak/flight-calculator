1afa60255b1c6575de36a0606394e3f1b623ce5c
Jacob <akintolajacob75@gmail.com>
2026-03-16 20:48:29 +0100

    feat: write conditional statements to give user travel destinations

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
-let destination = "Here are places your can go to:"
+let destinationText = "Here are places your can go to:";
+let destinationCity;
 
 function checkDestination() {
     const userAccountBalance = accountBalance.value;
-    console.log(userAccountBalance)
+
+    // Lagos
+    if (userAccountBalance >= 85000) {
+        destinationCity = "Lagos";
+        result.innerHTML = destinationText + "</br>* " + destinationCity;
+
+        // Benin City | ₦90,000
+        if (userAccountBalance >= 90000) {
+            destinationCity = "Benin City";
+            result.innerHTML += "</br>* " + destinationCity;
+
+            // Abuja
+            if (userAccountBalance >= 95000) {
+                destinationCity = "Abuja";
+                result.innerHTML += "</br>* " + destinationCity;
+
+                // Owerri
+                if (userAccountBalance >= 100000) {
+                    destinationCity = "Owerri";
+                    result.innerHTML += "</br>* " + destinationCity;
+
+                    // Enugu
+                    if (userAccountBalance >= 105000) {
+                        destinationCity = "Enugu";
+                        result.innerHTML += "</br>* " + destinationCity;
+
+                        // Port Harcourt
+                        if (userAccountBalance >= 110000) {
+                            destinationCity = "Port Harcourt";
+                            result.innerHTML += "</br>* " + destinationCity;
+
+                            // Uyo
+                            if (userAccountBalance >= 115000) {
+                                destinationCity = "Uyo";
+                                result.innerHTML += "</br>* " + destinationCity;
+
+                                // Calabar | ₦120,000
+                                if (userAccountBalance >= 120000) {
+                                    destinationCity = "Calabar";
+                                    result.innerHTML += "</br>* " + destinationCity;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+    } else {
+        result.innerHTML =
+            "Sorry, you don't have enough money to travel. <br>Just stay at home";
+    }
 }
